97b7bb0690bf9585ce9fa7a5f1f5405c45dec843
Erica Michelle <helloericakan@gmail.com>
2026-09-25 15:50:22 -0400

    Add form submission handler to create and display tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.getElementById("create-task-form");
+        form.addEventListener("submit", (e) => {
+          e.preventDefault();
+
+          const newTask = e.target.elements["new-task-description"].value;
+
+          buildToDo(newTask);
+        });
+
+        function buildToDo(task) {
+          const li = document.createElement("li");
+          li.textContent = task;
+
+          const taskList = document.getElementById("tasks");
+          taskList.appendChild(li);
+        }
